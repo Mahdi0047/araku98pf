b971a2cd354ff4e5931a4721eebcf93b563afa0f
mahdi jafari <m.jafari1379@gmail.com>
2019-12-01 12:20:16 +0330

    Update q03.cpp

--- s03/q03.cpp
+++ s03/q03.cpp
@@ -5,7 +5,11 @@ using namespace std;
 bool isprime(int n){
     if (n == 1)
         return false;
-    for (int i = 2; i < n; i++){
+	if (n == 2)
+		return true;
+	if (n % 2 == 0)
+		return false;
+    for (int i = 3; i < n; i += 2){
         if (n % i == 0){
             return false;
         }
