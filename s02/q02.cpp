636e8978c8a996a730f4a30008ba9a1febceec6b
mahdi jafari <m.jafari1379@gmail.com>
2019-12-01 12:02:07 +0330

    Create q02.cpp

---
+++ s02/q02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string date;
+    cin >> date;
+
+    if (date.length() == 4){
+        cout << "saal:" << date[0] << date[1] << endl << "maah:" << date[2] << date[3] << endl;
+    }else{
+        cout << "invalid!\n";
+    }
+
+    //system("pause");
+    //return 0;
+}
