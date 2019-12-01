1f59ab2f70ef3327c1c781283fd160ef1955e5fc
mahdi jafari <m.jafari1379@gmail.com>
2019-12-01 12:02:31 +0330

    Create q03.cpp

---
+++ s02/q03.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rib;
+    int outspace, inspace;
+    inspace = -1;
+    cin >> rib;
+
+    if (rib >= 3 && rib <= 20){
+        for (outspace = rib - 1; outspace >= 0; outspace--){
+            for (int i = 1; i <= 2; i++){
+                for (int j = 1; j <= outspace; j++){
+                    cout << ' ';
+                }
+                if (outspace == rib - 1){
+                    cout << '*';
+                }else{
+                    cout << '*';
+                    for (int j = 1; j <= inspace; j++){
+                        cout << ' ';
+                    }
+                    cout << '*';
+                }
+                for(int j = 1; j <= outspace; j++){
+                    cout << ' ';
+                }
+                if(i == 1){
+                    cout << ' ';
+                }
+            }
+            inspace += 2;
+            cout << endl;
+        }
+        inspace -= 4;
+        outspace += 2;
+        for (outspace; outspace <= rib - 1; outspace++){
+            for (int i = 1; i <= 2; i++){
+                for (int j = 1; j <= outspace; j++){
+                    cout << ' ';
+                }
+                if (outspace == rib - 1){
+                    cout << '*';
+                }else{
+                    cout << '*';
+                    for (int j = 1; j <= inspace; j++){
+                        cout << ' ';
+                    }
+                    cout << '*';
+                }
+                for (int j = 1; j <= outspace; j++){
+                    cout << ' ';
+                }
+                if(i == 1){
+                    cout << ' ';
+                }
+            }
+            inspace -= 2;
+            cout << endl;
+        }
+    }else{
+        cout << "#\n";
+    }
+
+    //system("pause");
+    //return 0;
+}
