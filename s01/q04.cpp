fd00776186c0a1161ffe06d0a10de6b84fde588d
mahdi jafari <m.jafari1379@gmail.com>
2019-12-01 11:59:38 +0330

    Update q04.cpp

--- s01/q04.cpp
+++ s01/q04.cpp
@@ -8,16 +8,17 @@ int main(){
     //cout << "enter desired number: ";
     cin >> number;
 	
-        for (int i = 1; i < number; i++){
-            if (number % i == 0){
-                sum += i;
-            }
+    for (int i = 1; i < number; i++){
+        if (number % i == 0){
+            sum += i;
         }
-        if (sum == number){
-            cout << "YES";
-        }else{
-            cout << "NO";
-        }
+    }
+    if (sum == number){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+	
     //system("pause");
     //return 0;
 }
