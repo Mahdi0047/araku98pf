e00d0b0ec5d1e5182755887b93de9b21ec5983da
mahdi jafari <m.jafari1379@gmail.com>
2019-12-01 12:01:24 +0330

    Create q01.cpp

---
+++ s02/q01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int data;
+    cin >> data;
+    cout << "W";
+
+    for(int i = 1; i <= data; i++){
+        cout << "o";
+    }
+
+    cout << "w!\n";
+
+    //system("pause");
+    //return 0;
+}
