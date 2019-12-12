e1aafab7f2d0ebed033e1121de5046f40348243a
mahdi jafari <m.jafari1379@gmail.com>
2019-12-13 01:12:32 +0330

    cleaned the code

--- s01/q02.cpp
+++ s01/q02.cpp
@@ -10,33 +10,29 @@ int main(){
     //cout << "enter the maximum number: ";
     cin >> b;
 
-    if (a >= 1 && a <= 10000 && b >= 1 && b <= 10000){
-        if (a > b){
-            int temp = a;
-            a = b;
-            b = temp;
-        }
+    if (a > b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
 
-        for (a; a <= b; a++){
-            if (a != 1){
-                prime = true;
-                if (a == 2){
+    for (a; a <= b; a++){
+        if (a != 1){
+            prime = true;
+            if (a == 2){
+                cout << a << endl;
+            }else {
+                for (int i = 2; i < a; i ++){
+                    if (a % i == 0){
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime){
                     cout << a << endl;
-                }else {
-                    for (int i = 2; i < a; i ++){
-                        if (a % i == 0){
-                            prime = false;
-                            break;
-                        }
-                    }
-                    if (prime){
-                        cout << a << endl;
-                    }
                 }
             }
         }
-    }else {
-        cout << "invalid!";
     }
     //system("pause");
     //return 0;
--- s01/q03.cpp
+++ s01/q03.cpp
@@ -7,19 +7,15 @@ int main(){
     //cout << "enter Rib length: "
     cin >> rib;
 
-    if(rib >= 1 && rib <= 10){
-        for (int i = 1; i <= rib; i++){
-            for (int j = 1; j<= rib; j++){
-                if (((i==1) || (i==rib)) || ((j==1) || (j==rib))){
-                    cout << "*";
-                }else {
-                    cout << " ";
-                }
+    for (int i = 1; i <= rib; i++){
+        for (int j = 1; j<= rib; j++){
+            if (((i==1) || (i==rib)) || ((j==1) || (j==rib))){
+                cout << "*";
+            }else {
+                cout << " ";
             }
-            cout << endl;
         }
-    }else{
-        cout << "invalid!";
+        cout << endl;
     }
     //system("pause");
     //return 0;
